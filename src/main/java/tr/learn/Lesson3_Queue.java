0a11177e7f92248bf5613258a4045d99081cb712
almir <madarat87@gmail.com>
2022-05-15 18:23:29 +0300

    Queue

---
+++ src/main/java/tr/learn/Lesson3_Queue.java
@@ -0,0 +1,28 @@
+package tr.learn;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Lesson3_Queue {
+    static Queue<Integer> q = new PriorityQueue<>();
+
+    public static void main(String[] args) {
+        q.add(2);
+        q.add(3);
+        System.out.println(q.peek());
+        q.poll();
+        System.out.println(q.peek());
+        q.poll();
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        System.out.println(q.peek());
+        q.poll();
+        System.out.println(q.peek());
+        q.poll();
+        System.out.println(q.peek());
+        q.poll();
+    }
+
+}

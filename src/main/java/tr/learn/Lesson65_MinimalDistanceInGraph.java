2737c06099669c1daf0036ab7ec6d1932ea14a85
almir <madarat@mail.ru>
2022-06-22 19:05:34 +0300

    Calculate minimal distance in graph

--- src/main/java/tr/learn/Lesson65_MinimalDistanceInGraph.java
+++ src/main/java/tr/learn/Lesson65_MinimalDistanceInGraph.java
@@ -10,15 +10,11 @@ public class Lesson65_MinimalDistanceInGraph {
     static void bfsList(int[][] graph, int startVertex) {
         List<Integer> used = new ArrayList<>(graph.length);
         for (int i = 0; i < graph.length; i++) {
-            used.add(0);
+            used.add(-1);
         }
-        used.set(startVertex, 1);
+        used.set(startVertex, 0);
         Queue<Integer> q = new LinkedList<>();
         q.add(startVertex);
-        List<Integer> distance = new ArrayList<>(graph.length);
-        for (int i = 0; i < graph.length; i++) {
-            distance.add(0);
-        }
 
         while (!q.isEmpty()) {
             int currentVertex = q.peek();
@@ -26,15 +22,15 @@ public class Lesson65_MinimalDistanceInGraph {
             System.out.print(currentVertex + 1 + " ");
             for (int i = 0; i < graph[currentVertex].length; i++) {
                 int adjacentVertex = graph[currentVertex][i];
-                if (used.get(adjacentVertex) != 1) {
+                if (used.get(adjacentVertex) == -1) {
                     q.add(adjacentVertex);
-                    distance.set(adjacentVertex, distance.get(currentVertex) + 1);
-                    used.set(adjacentVertex, 1);
+                    used.set(adjacentVertex, used.get(currentVertex) + 1);
                 }
             }
         }
-        for (int i = 0; i < distance.size(); i++) {
-            System.out.println(i + 1 + " " + distance.get(i));
+        System.out.println();
+        for (int i = 0; i < used.size(); i++) {
+            System.out.println(i + 1 + " " + used.get(i));
             
         }
     }
@@ -51,7 +47,7 @@ public class Lesson65_MinimalDistanceInGraph {
                 {4, 5, 6, 8},
                 {7}
         };
-
-        bfsList(graph, 0);
+        int startVertex = 1;
+        bfsList(graph, startVertex);
     }
 }

924578e9bd4f22a55f352dd69bb4c77a667021f3
almir <madarat@mail.ru>
2023-02-18 08:27:48 +0300

    Fibonacci recursion fixed

--- src/main/java/tr/learn/alishev/Fibonacci.java
+++ src/main/java/tr/learn/alishev/Fibonacci.java
@@ -1,28 +1,26 @@
 package tr.learn.alishev;
 
+import java.util.Arrays;
+
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fibNaive(3));
-        System.out.println(fibNaive(5));
-        System.out.println(fibEffective(10));
-        System.out.println(fibEffective(100));
+        int n = 100;
+        long[] mem = new long[n + 1];
+        Arrays.fill(mem, -1);
+
+        System.out.println(fibMem(n, mem));
     }
 
     // наивный, медленный, очевидный
-    private static long fibNaive(int n) {
+    private static long fibMem(int n, long[] mem) {
+        if (mem[n] != -1) {
+            return mem[n];
+        }
         if (n <= 1) {
             return n;
         }
-        return fibNaive(n - 1) + fibNaive(n - 2);
-    }
-
-    private static long fibEffective(int n) {
-        long[] arr = new long[n + 1];
-        arr[0] = 0;
-        arr[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            arr[i] = arr[i - 1] + arr[i - 2];
-        }
-        return arr[n];
+        long result = fibMem(n - 1, mem) + fibMem(n - 2, mem);
+        mem[n] = result;
+        return result;
     }
 }
